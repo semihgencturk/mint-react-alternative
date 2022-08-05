6ef131982bc7e883a4a689dea46820295294af3b
Semih Gencturk <semihgencturk@gmail.com>
2022-08-05 16:27:20 +0300

    Fourth commit

--- src/ui/Logo.js
+++ src/ui/Logo.js
@@ -4,9 +4,6 @@ import styled from "styled-components";
 const LogoContainer = styled.img`
   width: ${(props) => props.width && props.width}px;
   height: auto;
-  @media (max-height: 850px) {
-    display: none;
-  }
 `;
 
 const Logo = ({ src, alt, width }) => {
